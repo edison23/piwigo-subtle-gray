c69fc2a3b5c7c4e6982f5b29804b9d72648fa096
edison23 <klementdavid@gmail.com>
2018-06-17 17:07:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# piwigo-subtle-gray
---
+++ themeconf.inc.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Theme Name: Subtle gray
+Version: 0.1
+Description: Gray Piwigo theme based on default theme, color combinations inspired by Lightroom
+Theme URI: http://piwigo.org/ext/extension_view.php?eid=XXX
+Author: edison23
+Author URI: https://edison23.net/photography
+*/
+$themeconf = array(
+  'name'          => 'subtle-gray',
+  'parent'        => 'default'
+);
+?>
